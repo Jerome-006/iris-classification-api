86740104635cdb3f902341c576025d804a671251
Jerome-006 <jeromedhanapal@gmail.com>
2026-08-25 10:53:16 +0530

    Complete Task 5 API prediction

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+import joblib
+import numpy as np
 
 app = FastAPI()
 
+# load model only once
+model = joblib.load("ml/saved_model/model.joblib")
+
 @app.get("/")
-def root():
-    return{"message":"ML API is alive"}
+def home():
+    return {"message":"iris API running"}
 
 @app.post("/predict")
-def predict():
-    return{"prediction":"hardcoded_result"}
+def predict(data:dict):
+    features = [
+        [
+           data["sepal_length"],
+           data["sepal_width"],
+           data["petal_length"],
+           data["petal_width"]
+        ]
+    ]
+    prediction = model.predict(features)
+    return{"prediction":int(prediction[0])}
